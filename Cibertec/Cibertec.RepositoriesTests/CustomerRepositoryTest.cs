6cdb60d2757916e1b92a11ffe926d37af1532708
Juan Cucho <juan.cucho@outlook.com>
2017-09-16 14:05:59 -0500

    Se agrego pruebas unitarias

---
+++ Cibertec/Cibertec.RepositoriesTests/CustomerRepositoryTest.cs
@@ -0,0 +1,27 @@
+using Cibertec.Models;
+using Cibertec.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Xunit;
+
+namespace Cibertec.RepositoriesTests
+{
+    public class CustomerRepositoryTest
+    {
+        private readonly DbContext _context;
+
+        public CustomerRepositoryTest()
+        {
+            _context = new NorthwindDbContext();
+        }
+
+        [Fact(DisplayName = "[CustomerRepository] Get All")]
+        public void Customer_Repository_GetAll()
+        {
+            var repo = new RepositoryEF<Customer>(_context);
+            var result = repo.GetList();
+            //result.
+            Assert.True(result.Count() > 0);
+        }
+    }
+}
---
+++ Cibertec/Cibertec.RepositoriesTests/NorthwindDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Cibertec.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cibertec.RepositoriesTests
+{
+    public class NorthwindDbContext : DbContext
+    {
+        //configurando la tabla y el mapeo correspondiente 
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server =.; Database = Northwind_Lite; Trusted_Connection = True; MultipleActiveResultSets = True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+        }
+    }
+}
