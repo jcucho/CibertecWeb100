91d115dc292552329f3754f0453025fda6f92ffd
Juan Cucho <juan.cucho@outlook.com>
2017-11-12 19:27:50 -0500

    Cambios conexion base de datos local casa

--- Cibertec/Cibertec.Repositories.DapperTests/CustomerRepositoryTest.cs
+++ Cibertec/Cibertec.Repositories.DapperTests/CustomerRepositoryTest.cs
@@ -12,24 +12,24 @@ namespace Cibertec.Repositories.DapperTests
 
         public CustomerRepositoryTest()
         {
-            repo = new CustomerRepository("Server=MUNDODESA02;User Id=mundodesa02;Password=123;Database=Northwind_Lite");
+            repo = new CustomerRepository("Server=JUAN-PC\\MSSQLSERVER14;Database=Northwind_Lite; Trusted_Connection=True;MultipleActiveResultSets=True");
         }
 
-        [Fact(DisplayName = "[CustomerRepository]Get All")]
+        [Fact(DisplayName = "[CustomerRepository Dapper]Get All")]
         public void Customer_Repository_GetAll()
         {
             var result = repo.GetList();
             Assert.True(result.Count() > 0);
         }
 
-        [Fact(DisplayName = "[CustomerRepository]Insert")]
+        [Fact(DisplayName = "[CustomerRepository Dapper]Insert")]
         public void Customer_Repository_Insert()
         {
             var customer = GetNewCustomer();
             var result = repo.Insert(customer);
             Assert.True(result > 0);
         }
-        [Fact(DisplayName = "[CustomerRepository]Delete")]
+        [Fact(DisplayName = "[CustomerRepository Dapper]Delete")]
         public void Customer_Repository_Delete()
         {
             var customer = GetNewCustomer();
@@ -49,7 +49,7 @@ namespace Cibertec.Repositories.DapperTests
             };
         }
 
-        [Fact(DisplayName = "[CustomerRepository]Update")]
+        [Fact(DisplayName = "[CustomerRepository Dapper]Update")]
         public void Customer_Repository_Update()
         {
             var customer = repo.GetById(10);
@@ -58,7 +58,7 @@ namespace Cibertec.Repositories.DapperTests
             Assert.True(repo.Update(customer));
         }
 
-        [Fact(DisplayName = "[CustomerRepository]Get By Id")]
+        [Fact(DisplayName = "[CustomerRepository Dapper]Get By Id")]
         public void Customer_Repository_Get_By_Id()
         {
             var customer = repo.GetById(10);
--- Cibertec/Cibertec.Repositories.EntityFrameworkTests/NorthwindDbContext.cs
+++ Cibertec/Cibertec.Repositories.EntityFrameworkTests/NorthwindDbContext.cs
@@ -11,7 +11,7 @@ namespace Cibertec.Repositories.EntityFrameworkTests
         public DbSet<Customer> Customers { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=MUNDODESA02;User Id=mundodesa02;Password=123;Database=Northwind_Lite");
+            optionsBuilder.UseSqlServer("Server=JUAN-PC\\MSSQLSERVER14;Database=Northwind_Lite; Trusted_Connection=True;MultipleActiveResultSets=True");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
